c3cd6d889b76bdca26df5c257b848c176263ed3b
Felix Prasanna <fpx@mit.edu>
2024-01-22 23:24:47 -0500

    make encrypt tail recursive
    saves 3

--- solitaire-fprasx.py
+++ solitaire-fprasx.py
@@ -1,20 +1,19 @@
 import random
 
+# golf start
+A=53
+def k(d):p=lambda j,d:A>(k:=d.index(j))and d[:k]+[d[k+1],j]+d[k+2:]or[d[0],j]+d[1:k];d=p(54,p(54,p(A,d)));l,h=sorted([d.index(A),d.index(54)]);d=d[h+1:]+d[l:h+1]+d[:l];b=min(d[-1],A);d=d[b:-1]+d[:b]+[d[-1]];t=min(d[0],A);return A>d[t]and(d[t],d)or k(d)
+def encrypt(s,d):o,d=k(d);return s and chr(97+(ord(s[0])-97+o)%26)+encrypt(s[1:],d)or""
+# golf end
+
+
+expected = "rvwszsylhrnfpkpeetpooeuqenlkhfynbncoohblhadptklqqkliqggoddcwqcfgxsazququpdzjkeqgcsachmjskaxcxqheawtwqymadnibxsxylrtkahsrwwvlcfhf"
+plaintext = "mvdpbacxvvlpmeabquboslrcrxdaqfedkywedhybmpanzabutgjdahejnkhrgsbaxczydcdyuinxkptidzwlnocetmjnwxekzzyjyhsgjkuecfsicwprfpgfsmqkzkki"
 d = [
     1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,
     29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54
     ]
 random.seed(5318008)
 random.shuffle(d)
-
-# golf start
-A=53
-p=lambda j:A>(k:=d.index(j))and d[:k]+[d[k+1],j]+d[k+2:]or[d[0],j]+d[1:k]
-def k():global d;d=p(A);d=p(54);d=p(54);l,h=sorted([d.index(A),d.index(54)]);d=d[h+1:]+d[l:h+1]+d[:l];b=min(d[-1],A);d=d[b:-1]+d[:b]+[d[-1]];t=min(d[0],A);return A>d[t]and d[t]or k()
-encrypt=lambda s:"".join(chr(97+(ord(c)-97+k())%26)for c in s)
-# golf end
-
-expected = "rvwszsylhrnfpkpeetpooeuqenlkhfynbncoohblhadptklqqkliqggoddcwqcfgxsazququpdzjkeqgcsachmjskaxcxqheawtwqymadnibxsxylrtkahsrwwvlcfhf"
-plaintext = "mvdpbacxvvlpmeabquboslrcrxdaqfedkywedhybmpanzabutgjdahejnkhrgsbaxczydcdyuinxkptidzwlnocetmjnwxekzzyjyhsgjkuecfsicwprfpgfsmqkzkki"
-result = encrypt(plaintext)
+result = encrypt(plaintext, d)
 assert result == expected, f"{result}"
