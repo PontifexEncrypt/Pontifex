16aa95f55a4cd93f7954cc896fe10fab0a30507b
Felix Prasanna <fpx@mit.edu>
2024-01-22 20:48:57 -0500

    ultra compressify

--- solitaire-fprasx.py
+++ solitaire-fprasx.py
@@ -10,25 +10,8 @@ random.shuffle(d)
 # golf start
 i=lambda x:d.index(x)
 p=lambda j:53>(k:=i(j))and d[:k]+[d[k+1],j]+d[k+2:]or[d[0],j]+d[1:k]
-
 def k():
-    global d
-    d=p(53)
-    d=p(54)
-    d=p(54)
-
-    # triple cut
-    lo,hi=sorted([i(53),i(54)])
-    d=d[hi+1:]+d[lo:hi+1]+d[:lo]
-
-    # count end
-    b=min(d[-1],53)
-    d=d[b:-1]+d[:b]+[d[-1]]
-
-    # return value
-    t=min(d[0],53)
-    return 53>d[t]and d[t]or k()
-    
+ global d;d=p(53);d=p(54);d=p(54);lo,hi=sorted([i(53),i(54)]);d=d[hi+1:]+d[lo:hi+1]+d[:lo];b=min(d[-1],53);d=d[b:-1]+d[:b]+[d[-1]];t=min(d[0],53);return 53>d[t]and d[t]or k()
 # golf end
 
 expected = "rvwszsylhrnfpkpeetpooeuqenlkhfynbncoohblhadptklqqkliqggoddcwqcfgxsazququpdzjkeqgcsachmjskaxcxqheawtwqymadnibxsxylrtkahsrwwvlcfhf"
