68832bae4fb12bba8f9c74b509c55374055406c0
Felix Prasanna <fpx@mit.edu>
2024-01-25 16:58:09 -0500

    use slicing instead of indexing
    getting the first and last character is equivalent to using the length
    of the list - 1 as the stride

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rg --multiline '(?s)# golf start\n(.*)\n# golf end' -or '$1' solitaire-fprasx.py | wc -c
+rg --multiline '(?s)# golf start\n(.*)\n# golf end' -or '$1' solitaire-golf-fprasx.py | wc -c
 
--- solitaire-golf-fprasx.py
+++ solitaire-golf-fprasx.py
@@ -1,7 +1,7 @@
 import random
 
 # golf start
-p=lambda j,d:53>(k:=d.index(j))and d[:k]+[d[k+1],j]+d[k+2:]or[d[0],j]+d[1:k]
+p=lambda j,d:53>(k:=d.index(j))and d[:k]+[d[k+1],j]+d[k+2:]or d[::k]+d[1:k]
 def e(s,d,t=""):d=p(54,p(54,p(53,d)));l,h=sorted([d.index(53),d.index(54)]);d=d[h+1:]+d[l:h+1]+d[:l];b=d[-1];d=d[b:-1]+d[:-1][:b]+[b];o=d[d[0]%54or-1];return s and(o<53and e(s[1:],d,t+chr((ord(s[0])-97+o)%26+97))or e(s,d,t))or t
 encrypt=e
 # golf end
